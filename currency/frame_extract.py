677db2b76779a32bafe46d048e5a18c85892edca
sam189239 <sathiyamurthi239@gmail.com>
2022-02-22 12:41:22 +0530

    update

--- currency/frame_extract.py
+++ currency/frame_extract.py
@@ -1,19 +1,20 @@
 import cv2
  
 # Opens the Video file
-cap= cv2.VideoCapture('Play.mp4')
+cap= cv2.VideoCapture('data/vids/50.mp4')
 i=0
 j=0
-note = 'ten'
+note = 'fifty'
+dir = 'data/frames/'+note+'/'
 while(cap.isOpened()):
     ret, frame = cap.read()
     if ret == False:
         break
     if i%1 == 0:
-        print(cv2.Laplacian(frame, cv2.CV_64F).var())
-        # if cv2.Laplacian(frame, cv2.CV_64F).var() >= 200:
-            # cv2.imwrite(note+str(j)+'.jpg',frame)
-            # j+=1
+        # print(cv2.Laplacian(frame, cv2.CV_64F).var())
+        if cv2.Laplacian(frame, cv2.CV_64F).var() >= 450:
+            cv2.imwrite(dir+note+str(j)+'.jpg',frame)
+            j+=1
     i+=1
     
  
